d1afb5b7bdc8700d768aa290f0423fcb40232de7
Luis Ernesto Muñoz Martinez <ernestom099@outlook.com>
2022-06-07 23:41:29 -0500

    Update: Uploading unversioned files

---
+++ app/src/main/java/com/example/codechallenge/mainModule/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.codechallenge.mainModule
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.codechallenge.common.utis.EuroPrice
+import com.example.codechallenge.mainModule.model.MainInteractor
+
+class MainViewModel : ViewModel() {
+
+    private val euroPrice: MutableLiveData<EuroPrice> by lazy {
+        MutableLiveData<EuroPrice>().also {
+            loadOrders()
+        }
+    }
+
+    private val mainInteractor by lazy {
+        MainInteractor()
+    }
+
+    fun getEuroPrices(): MutableLiveData<EuroPrice> = euroPrice
+
+
+    fun loadOrders() {
+        mainInteractor.getEuroPrice {
+            with(it) {
+                euroPrice.postValue(EuroPrice.invoke(USD, GBP, AUD))
+            }
+        }
+    }
+}
